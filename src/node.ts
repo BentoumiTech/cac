f59fd6021e1e5f748ba278428efa61743feea87e
Khaled Bentoumi <kbentoumi@gmail.com>
2020-05-13 13:17:26 +0200

    fix: Window path name not exist

--- src/node.ts
+++ src/node.ts
@@ -2,14 +2,10 @@ declare let window: any
 declare let Deno: any
 
 const deno = typeof window !== 'undefined' && window.Deno
-const denoScriptPath =
-  deno && typeof window !== 'undefined' && window.location.pathname
 
 // Adds deno executable and script path to processArgs as "compatibility" layer for node
 // See https://github.com/cacjs/cac/issues/69
-export const processArgs = deno
-  ? ['deno', denoScriptPath].concat(Deno.args)
-  : process.argv
+export const processArgs = deno ? ['deno', ' '].concat(Deno.args) : process.argv
 
 export const platformInfo = deno
   ? `${Deno.build.os}-${Deno.build.arch} deno-${Deno.version.deno}`
